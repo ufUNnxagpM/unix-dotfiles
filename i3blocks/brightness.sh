204cc54962a4fb003a744437f17958747cd0dc01
Chromo-residuum-opec <12422133+ufUNnxagpM@users.noreply.github.com>
2015-11-16 19:54:36 -0800

    added scroll support for i3blocks

--- i3blocks/brightness.sh
+++ i3blocks/brightness.sh
@@ -2,4 +2,10 @@
 
 backlight=$(xbacklight)
 echo $(python -c "print (round(${backlight}))")
+
+case $BLOCK_BUTTON in
+  4) xbacklight +1 ;; # scroll up, increase
+  5) xbacklight -1 ;; # scroll down, decrease
+esac
+
 exit 0
--- i3blocks/mpd.sh
+++ i3blocks/mpd.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 #check variable counts the word playing in output of mpc
 check=$(mpc | grep -c playing)
+mout=$(mpc | sed -n '1p')
 if [ $check -gt 0 ]
 then
-mout=$(mpc | sed -n '1p')
-mout="⮕  $mout "
-echo $mout
+mout="⮓ $mout " #"<span foreground="#95c7ae"> hello </span>"
+elif [ $(pidof ncmpcpp) -gt 0 ] #use this option if you only want it to display when ncmpcpp is open
+#elif [ $(mpc | grep -c paused) -gt 0 ] #use this option if you want it display at all times
+then
+mout="⮔ $mout "
 else
-echo " "
+mout=" "
 fi
-exit 0
+echo $mout
 
+case $BLOCK_BUTTON in
+  3) mpc toggle ;;  # right click, mute/unmute
+  4) mpc prev ;; # scroll up, previous song
+  5) mpc next ;; # scroll down, next song
+esac
+
+exit 0
