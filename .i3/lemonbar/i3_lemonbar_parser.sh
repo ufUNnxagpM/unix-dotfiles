2347988564d3ab4b66442e392562eaaed8486c5c
BuildTools <unconfigured@null.spigotmc.org>
2017-01-15 03:14:19 -0800

    updated multiple configs (mainly qutebrowser and mpv)

--- .i3/lemonbar/i3_lemonbar_parser.sh
+++ .i3/lemonbar/i3_lemonbar_parser.sh
@@ -24,7 +24,7 @@ while read -r line ; do
       fi
       date="%{F${color_sec_b1}}${sep_left}%{F${color_icon} B${color_sec_b1}} %{T2}${icon_clock}%{F- T1} ${date}"
       # time
-      time="%{F${color_head}}${sep_left}%{F${color_back} B${color_head}} ${sys_arr[3]} %{F- B-}"
+      time="%{F${color_head}}${sep_left}%{F${color_fore} B${color_head}} ${sys_arr[3]} %{F- B-}"
       # cpu
       if [ ${sys_arr[4]} -gt ${cpu_alert} ]; then
         cpu_cback=${color_cpu}; cpu_cicon=${color_back}; cpu_cfore=${color_back};
@@ -75,10 +75,11 @@ while read -r line ; do
       # Gmail
       gmail="${line#???}"
       if [ "${gmail}" != "0" ]; then
-        mail_cback=${color_mail}; mail_cicon=${color_back}; mail_cfore=${color_back}
+        mail_cback=${color_mail}; mail_cicon=${color_fore}; mail_cfore=${color_fore}
       else
-        mail_cback=${color_sec_b1}; mail_cicon=${color_icon}; mail_cfore=${color_fore}
+        mail_cback=${color_sec_b3}; mail_cicon=${color_icon}; mail_cfore=${color_fore}
       fi
+      #gmail="%{F${color_fore}}${sep_l_left}%{F${mail_cback}}%{F${sep_left}${mail_cicon} B${mail_cback}} %{T2}${icon_mail}%{F${mail_cfore} T1} ${gmail}"
       gmail="%{F${mail_cback}}${sep_left}%{F${mail_cicon} B${mail_cback}} %{T2}${icon_mail}%{F${mail_cfore} T1} ${gmail}"
       ;;
     IRC*)
@@ -123,12 +124,12 @@ while read -r line ; do
     WIN*)
       # window title
       title=$(xprop -id ${line#???} | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-      title="%{F${color_head} B${color_sec_b2}}${sep_right}%{F${color_head} B${color_sec_b2}%{T2} ${icon_prog} %{F${color_sec_b2} B-}${sep_right}%{F- B- T1} ${title}"
+      title="%{F${color_head} B${color_sec_b2}}${sep_right}%{F${color_fore} B${color_sec_b2}%{T2} ${icon_prog} %{F${color_sec_b2} B-}${sep_right}%{F- B- T1} ${title}"
       #title="%{F${color_head} B${color_sec_b2}}${sep_right}%{F${color_head} B${color_sec_b2} T2} ${icon_prog} %{F${color_sec_b2} B-}${sep_right}%{F- B- T1} ${title}"
       ;;
   esac
 
   # And finally, output
-  printf "%s\n" "%{l}${wsp}${title} %{r}${mpd}${stab}${irc}${stab}${gmail}${stab}${cpu}${stab}${mem}${stab}${diskr}${stab}${diskh}${stab}${wland}${stab}${wlanu}${stab}${ethd}${stab}${ethu}${stab}${vol}${stab}${date}${stab}${time}"
+  printf "%s\n" "%{l}${wsp}${title} %{r}${mpd}${stab}${gmail}${stab}${cpu}${stab}${mem}${stab}${ethd}${stab}${ethu}${stab}${vol}${stab}${date}${stab}${time}"
   #printf "%s\n" "%{l}${wsp}${title}"
 done
