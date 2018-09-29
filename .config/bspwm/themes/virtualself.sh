bc0cf1a7ca4855f8eaf34eb2b1efd39d9e41a02a
Chromo-residuum-opec <12422133+ufUNnxagpM@users.noreply.github.com>
2018-09-29 11:25:37 -0700

    theme updates

--- .config/bspwm/themes/virtualself.sh
+++ .config/bspwm/themes/virtualself.sh
@@ -1,7 +1,7 @@
-bspc config normal_border_color #30302f
-bspc config active_border_color #474645
-bspc config focused_border_color #817f7f
-bspc config presel_feedback_color #f4d775
+bspc config normal_border_color \#30302f
+bspc config active_border_color \#474645
+bspc config focused_border_color \#817f7f
+bspc config presel_feedback_color \#f4d775
 
 bspc config border_width         3
 bspc config window_gap          40
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,13 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch top and bottom with custom theme
+if [[ $# -ne 1 ]]; then
+	polybar top &
+	polybar bottom &
+	echo "WARNING: No theme applied to polybar"
+else
 polybar -c $HOME/.config/polybar/themes/$THEME top &
 polybar -c $HOME/.config/polybar/themes/$THEME bottom &
+fi 
 
 echo "Bars launched..."
