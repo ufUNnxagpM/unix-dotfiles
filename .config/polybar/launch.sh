c6cc3e1eeccc423630e29b5a0ebf8b4966f245b3
Chromo-residuum-opec <12422133+ufUNnxagpM@users.noreply.github.com>
2021-02-19 01:38:02 -0800

    Update polybar launcher script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,17 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch top and bottom with custom theme
-if [[ $# -ne 1 ]]; then
-	polybar top &
-	polybar bottom &
-	polybar ext &
-	echo "WARNING: No theme applied to polybar"
-else
-	polybar -c $HOME/.config/polybar/themes/$THEME top &
-	polybar -c $HOME/.config/polybar/themes/$THEME bottom &
-	polybar -c $HOME/.config/polybar/themes/$THEME right &
-	polybar -c $HOME/.config/polybar/themes/$THEME left &
-fi 
+BAR_NAMES=( top bottom left right )
 
-echo "Bars launched..."
+# Launch top and bottom with custom theme
+for i in "${BAR_NAMES[@]}"
+do
+	if [[ $# -ne 1 ]]; then
+		polybar $i &
+	else
+		polybar -c $HOME/.config/polybar/themes/$THEME $i &
+	fi
+done
+
+echo $'\e[1;33m'Bars launched with theme $THEME$'\e[0m'
