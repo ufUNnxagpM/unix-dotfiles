75916b4901ea4067b895dfdcebac35b67d408aeb
Matthew Treadwell <hewtreadwell@gmail.com>
2021-05-05 17:51:30 -0700

    Add new rofi themes; update qutebrowser, zsh, and mpv

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -81,23 +81,26 @@ config.bind('C', 'spawn chromium {url}')
 config.bind('R', 'reload -f')
 
 c.content.blocking.enabled = False
+c.content.blocking.method = "adblock"
 
 import sys, os
 
 config.set(
-    "content.blocking.hosts.lists",
+    "content.blocking.adblock.lists",
     [
         "https://easylist.to/easylist/easylist.txt",
         "https://easylist.to/easylist/easyprivacy.txt",
         "https://easylist.to/easylist/fanboy-annoyance.txt",
+        "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_4_Social/filter.txt",
+        "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt",
         "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/resource-abuse.txt",
-        "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt",
-        "https://www.malwaredomainlist.com/hostslist/hosts.txt",
-        "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
+        #"https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/unbreak.txt",
+        #"https://www.malwaredomainlist.com/hostslist/hosts.txt",
+        #"https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&mimetype=plaintext",
         "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
     ],
 )
