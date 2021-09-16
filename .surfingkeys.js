950016dc0f70f5cb16eb593407eb93431fa34b5f
Chromo-residuum-opec <12422133+ufUNnxagpM@users.noreply.github.com>
2021-09-15 18:42:03 -0700

    Update

--- .surfingkeys.js
+++ .surfingkeys.js
@@ -1,4 +1,5 @@
 Hints.characters = "arstdqwfpgzxcvb";
+Hints.style('border: solid 3px #552a48; color:#efe1eb; background: initial; background-color: #552a48; font-family: sans-serif;');
 settings.smartCase = true;
 
 // Save keys
@@ -44,8 +45,11 @@ map("y", "R");
 map("d", "x");
 map("D", "X");
 map(".", "x");
-mapkey(',', '#3Close current tab', function() {
-	settings.focusAfterClosed = "left";
+mapkey(',', 'Close current tab and move left', function() {
+	// Move tab to left and close to right (achieves desired affect)
+    	RUNTIME('moveTab', {
+        	step: -1
+    	});
 	RUNTIME("closeTab");
 });
 
@@ -54,3 +58,49 @@ map("F", "af");
 
 // Reload page
 map("R", ">_r");
+
+// Turn off surfingkeys
+map('<Ctrl-i>', '<Alt-s>'); // hotkey must be one keystroke with/without modifier, it can not be a sequence of keystrokes like `gg`.
+
+// unmap alt toggles
+unmap("<Alt-i>");
+unmap("<Alt-s>");
+unmap("<Alt-p>");
+unmap("<Alt-m>");
+iunmap("<Alt-b>");
+iunmap("<Alt-f>");
+iunmap("<Alt-w>");
+iunmap("<Alt-d>");
+
+// set theme
+settings.theme = `
+.sk_theme {
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #282828;
+    color: #ebdbb2;
+}
+.sk_theme tbody {
+    color: #b8bb26;
+}
+.sk_theme input {
+    color: #d9dce0;
+}
+.sk_theme .url {
+    color: #98971a;
+}
+.sk_theme .annotation {
+    color: #b16286;
+}
+.sk_theme .omnibar_highlight {
+    color: #ebdbb2;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: #282828;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: #d3869b;
+}
+#sk_status, #sk_find {
+    font-size: 20pt;
+}`;
