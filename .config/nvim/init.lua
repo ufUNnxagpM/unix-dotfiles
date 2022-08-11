1f8fa10496d256ebf094cdd4800fc42902eb0030
Chromo-residuum-opec <12422133+ufUNnxagpM@users.noreply.github.com>
2022-08-11 02:10:34 -0700

    gitignore, nvim lua fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,7 @@ vim.o.number = true
 vim.o.smartcase = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.clipboard = unnamedplus
+vim.o.clipboard = "unnamedplus"
 
 -- colemak key bindings
 vim.cmd('source ~/.config/nvim/colemak.vim')
